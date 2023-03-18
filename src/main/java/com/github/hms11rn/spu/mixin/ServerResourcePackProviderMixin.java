460c9b82ba5f676eaabbd48d062d7c1260f2eec9
hms11rn <hmselik@gmail.com>
2023-03-18 23:20:45 +0200

    Added Mixin to disable alwaysEnabled

--- src/main/java/com/github/hms11rn/spu/mixin/ServerResourcePackProviderMixin.java
+++ src/main/java/com/github/hms11rn/spu/mixin/ServerResourcePackProviderMixin.java
@@ -1,20 +1,26 @@
 package com.github.hms11rn.spu.mixin;
 
-import net.minecraft.client.resource.ServerResourcePackProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+import net.minecraft.client.resource.ServerResourcePackProvider;
+
 @Mixin(ServerResourcePackProvider.class)
 public class ServerResourcePackProviderMixin {
 
-    @ModifyArg(
-            method = "loadServerPack(Ljava/io/File;Lnet/minecraft/resource/ResourcePackSource;)Ljava/util/concurrent/CompletableFuture;",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourcePackProfile;of(Ljava/lang/String;Lnet/minecraft/text/Text;ZLnet/minecraft/resource/ResourcePackProfile$PackFactory;Lnet/minecraft/resource/ResourcePackProfile$Metadata;Lnet/minecraft/resource/ResourceType;Lnet/minecraft/resource/ResourcePackProfile$InsertionPosition;ZLnet/minecraft/resource/ResourcePackSource;)Lnet/minecraft/resource/ResourcePackProfile;"),
-            index = 7
-    )
-    private boolean loadServerPack(boolean isPinned) {
-        return false;
-    }
+	@ModifyArg(method = "loadServerPack(Ljava/io/File;Lnet/minecraft/resource/ResourcePackSource;)Ljava/util/concurrent/CompletableFuture;",
+	at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourcePackProfile;of(Ljava/lang/String;Lnet/minecraft/text/Text;ZLnet/minecraft/resource/ResourcePackProfile$PackFactory;Lnet/minecraft/resource/ResourcePackProfile$Metadata;Lnet/minecraft/resource/ResourceType;Lnet/minecraft/resource/ResourcePackProfile$InsertionPosition;ZLnet/minecraft/resource/ResourcePackSource;)Lnet/minecraft/resource/ResourcePackProfile;"), 
+	index = 7)
+	private boolean loadServerPack(boolean isPinned) {
+		return false;
+	}
+
+	@ModifyArg(method = "loadServerPack(Ljava/io/File;Lnet/minecraft/resource/ResourcePackSource;)Ljava/util/concurrent/CompletableFuture;", 
+	at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourcePackProfile;of(Ljava/lang/String;Lnet/minecraft/text/Text;ZLnet/minecraft/resource/ResourcePackProfile$PackFactory;Lnet/minecraft/resource/ResourcePackProfile$Metadata;Lnet/minecraft/resource/ResourceType;Lnet/minecraft/resource/ResourcePackProfile$InsertionPosition;ZLnet/minecraft/resource/ResourcePackSource;)Lnet/minecraft/resource/ResourcePackProfile;"),
+	index = 3)
+	private boolean forcedEnabled(boolean enabled) {
+		return false;
+	}
 
 }
