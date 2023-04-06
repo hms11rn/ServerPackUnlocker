067e3eb893118f8cab249d46e453dc94b646088b
hms11rn <hmselik@gmail.com>
2023-04-07 00:13:43 +0300

    Added mixin to net.minecraft.client.gui.screen.pack.PackScreen

---
+++ src/main/java/com/github/hms11rn/spu/mixin/PackScreenMixin.java
@@ -0,0 +1,31 @@
+package com.github.hms11rn.spu.mixin;
+
+import com.github.hms11rn.spu.ServerPackUnlocker;
+import net.minecraft.client.gui.screen.pack.PackScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Collection;
+
+/**
+ * Used to get when the resource pack screen is closed
+ *
+ * @author hms11rn
+ */
+@Mixin(PackScreen.class)
+public class PackScreenMixin {
+
+    /**
+     * Hooks into close() and sets justClosedPackScreen to 1
+     * so {@link com.github.hms11rn.spu.mixin.ResourcePackManagerMixin#buildEnabledProfilesInjections(Collection, CallbackInfoReturnable)} knows
+     * that the build of enabled profiles is because resource pack screen was closed
+     * @param in not used
+     */
+    @Inject(method = "close", at = @At("HEAD"))
+    private void closedPackScreen(CallbackInfo in) {
+        ServerPackUnlocker.modInstance.justClosedPackScreen = 1;
+    }
+}
